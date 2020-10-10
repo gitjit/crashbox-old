c8d0e1bfcacddb04bc4b64cb54847c81f7c8ffbd
Jithesh <jithesh.chandrasekharan@hp.com>
2020-10-09 18:53:45 -0700

    testing slot

--- src/CrashBox.Api/CrashBox.Api.cs
+++ src/CrashBox.Api/CrashBox.Api.cs
@@ -62,7 +62,7 @@ namespace CrashBox.Api
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "crash")] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("CrashBox: Test Jithesh Get Crashes Called...!!!!");
+            log.LogInformation("JitCrashBox: Test Jithesh Get Crashes Called...!!!!");
             _logger = log;
             try
             {
